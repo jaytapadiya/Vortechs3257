8c3c2b87f73ffcb1b102b70de396c0bf2c97681c
jaytapadiya <jay.tapadiya@gmail.com>
2017-01-23 20:09:55 -0800

    added code for camera and cleaned up file

--- 2017/src/org/usfirst/frc/team3257/robot/Robot.java
+++ 2017/src/org/usfirst/frc/team3257/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team3257.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
@@ -8,6 +9,9 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import java.text.DecimalFormat;
+
 import org.usfirst.frc.team3257.robot.commands.ExampleCommand;
 import org.usfirst.frc.team3257.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -25,9 +29,7 @@ public class Robot extends IterativeRobot {
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
-    Command autonomousCommand;
-    SendableChooser chooser;
-    Joystick stick1;
+    Joystick stick, xbox;
     Jaguar FR, FL, BR, BL, ML, MR;
     
 
@@ -35,20 +37,21 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    @SuppressWarnings("deprecation")
+	public void robotInit() {
 		oi = new OI();
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
-        stick1 = new Joystick(0);
+        xbox = new Joystick(0);
+        stick = new Joystick(1);
         FR = new Jaguar(0);
         MR = new Jaguar(1);
         BR = new Jaguar(2);
         BL = new Jaguar(3);
         ML = new Jaguar(4);
         FL = new Jaguar(5);
-        
+        CameraServer server = CameraServer.getInstance();
+        //(CameraServer.kSize640x480);;
+        server.startAutomaticCapture();;
+
     }
     
 	/**
@@ -61,114 +64,71 @@ public class Robot extends IterativeRobot {
     }
 	
 	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
 	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
-	 * or additional comparisons to the switch structure below with additional strings & commands.
-	 */
     public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
-        
-		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
-		switch(autoSelected) {
-		case "My Auto":
-			autonomousCommand = new MyAutoCommand();
-			break;
-		case "Default Auto":
-		default:
-			autonomousCommand = new ExampleCommand();
-			break;
-		} */
-    	
-    	// schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
     }
 
     public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
     }
-
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
- //       Scheduler.getInstance().run();
-        
-        tankdrive();
+    normalDrive();
     }
     
-    public void tankdrive(){
-    	double fr;
-    	double fl;
-    	double br;
-    	double bl;
-    	double ml;
-    	double mr;
-    	double Yaxis = stick1.getY();
-    	double Xaxis = stick1.getX();
-    	
-//    	fr = Xaxis;
-//    	br = Xaxis;
-//    	mr = Xaxis;
-//    	fl = Yaxis;
-//    	bl = Yaxis;
-//    	ml = Yaxis;
-    	
-    	
-    	if((Math.abs(Xaxis) < 0.2) && (Math.abs(Yaxis) > 0.2)) {
-    		fl = Yaxis;
-    		ml = Yaxis;
-    		bl = Yaxis;
-    		fr = -Yaxis;
-    		mr = -Yaxis;
-    		br = -Yaxis;
-    	}
-    	else if((Math.abs(Xaxis) > 0.2) && (Math.abs(Yaxis) < 0.2)){
-    		fl = Xaxis;
-        	ml = Xaxis;
-        	bl = Xaxis;
-        	fr = Xaxis;
-        	mr = Xaxis;
-        	br = Xaxis;
-        	
-    	}
-    	
-        	else {
-        		fl = 0;
-        		ml = 0;
-        		bl = 0;
-        		fr = 0;
-        		mr = 0;
-        		br = 0;
-        	}
-        	
-    	
-    		FL.set(fl);
-    		FR.set(fr);
-    		BL.set(bl);
-    		BR.set(br);
-    		ML.set(ml);
-    		MR.set(mr);
-        	
-    }
+	public void normalDrive() {
+		double axisXinitial = xbox.getRawAxis(4); //axis 4 is right joystick x axis
+		DecimalFormat myFormat = new DecimalFormat("0.0");
+		double axisX = Double.parseDouble(myFormat.format(axisXinitial));
+		double leftStickYinitial = xbox.getY();
+		double leftStickY = Double.parseDouble(myFormat.format(leftStickYinitial));
+		double fl;
+		double bl;
+		double fr;
+		double br;
+		
+		double actualY = Math.pow(leftStickY, 7.0); //cubic function; makes sensitivity at lower magnitude less significant and exponentially increases
+		double actualX = 1 - (Math.abs(axisX))*.5; //this value will be set to the side of motors to which the robot is turning; the inner wheels can turn a maximum of half the speed of the outer wheels
+
+		
+		if ((Math.abs(axisX) < .2) && (Math.abs(actualY) > .2)) { //0.2 is used throughout to account for the inaccuracy of the joystick
+			fl = actualY;
+			bl = actualY;
+			fr = -actualY;
+			br = -actualY;
+		} else if (axisX > 0.2 && (Math.abs(actualY) > 0.2)) {
+			fl = actualY;
+			bl = actualY;
+			fr = -(actualY * actualX);
+			br = -(actualY * actualX);
+		} else if (axisX < -0.2 && (Math.abs(actualY) > 0.2)) {
+			fl = (actualY * actualX);
+			bl = (actualY * actualX);
+			fr = -actualY;
+			br = -actualY;
+		} else if ((Math.abs(actualY) < 0.2) && (Math.abs(axisX) > 0.2)) {
+			fl = -(axisX);
+			bl = -(axisX);
+			fr = -(axisX);
+			br = -(axisX);
+		} else	{
+			fl = 0;
+			fr = 0;
+			bl = 0;
+			br = 0;
+		}
+				
+			FL.set(-fl * .5); // multiply value by .5 to make 50% speed
+			BL.set(-bl * .5);
+			FR.set(-fr * .5);
+			BR.set(-br * .5);
+			ML.set(-fl * .5);
+			MR.set(-br * .5);
+		}
     
     /**
      * This function is called periodically during test mode
